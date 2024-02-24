41b7db371ce68953eef362c44bf5cadfb45d0180
Olafo0 <opaladzs@gmail.com>
2024-02-24 22:05:58 +0000

    Add project files.

---
+++ PostcodeAPITest/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Net.Http;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+
+namespace PostcodeAPITest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void SearchPostCodeBTN_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var client = new HttpClient();
+                var request = new HttpRequestMessage
+                {
+                    Method = HttpMethod.Get,
+                    RequestUri = new Uri($"https://uk-postcode.p.rapidapi.com/getpostcode?postcode={PostCodeTB.Text}"),
+                    Headers =
+                {
+                    { "X-RapidAPI-Key", "x" },
+                    { "X-RapidAPI-Host", "x" },
+                },
+                };
+
+                using (var response = await client.SendAsync(request))
+                {
+                    response.EnsureSuccessStatusCode();
+                    var body = await response.Content.ReadAsStringAsync();
+                    var data = JsonConvert.DeserializeObject<PostCode.Rootobject>(body);
+                    AddressesCB.Items.Add(data.result.streetName);
+                }
+            }
+            catch(Exception ex)
+            {
+                Window window = new Window();
+                window.Width = 200;
+                window.Height = 200;
+                window.Title = "ERROR";
+
+                Label tt = new Label()
+                {
+                    Content = new TextBlock()
+                    {
+                        Text = ex.Message,
+                        TextWrapping = TextWrapping.Wrap
+                    }
+                };
+
+                StackPanel container = new StackPanel();
+                container.Children.Add(tt);
+
+
+                window.ResizeMode = ResizeMode.NoResize;
+                window.Content = container;
+
+                window.Show();
+            }
+        }
+    }
+}
---
+++ PostcodeAPITest/PostCode.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostcodeAPITest
+{
+    internal class PostCode
+    {
+        public class Rootobject
+        {
+            public Result result { get; set; }
+        }
+
+        public class Result
+        {
+            public string postCode { get; set; }
+            public string postCodeTrimmed { get; set; }
+            public string streetName { get; set; }
+            public decimal longitude { get; set; }
+            public decimal latitude { get; set; }
+            public string plusCode { get; set; }
+            public string region { get; set; }
+            public string locality { get; set; }
+            public string area { get; set; }
+            public int numUPRNs { get; set; }
+        }
+
+        //public string postCode { get; set; }
+        //public string postCodeTrimmed { get; set; }
+        //public string streetName { get; set; }
+        //public int longitude { get; set; }
+        //public int latitude { get; set; }
+        //public string plusCode { get; set; }
+        //public string region { get; set; }
+        //public string locality { get; set; }
+        //public string area { get; set; }
+        //public int numUPRNs { get; set; }
+
+        //public PostCode(string postCode, string postCodeTrimmed, string streetName, int longitude, int latitude, string plusCode, string region, string locality, string area, int numUPRNs)
+        //{
+        //    this.postCode = postCode;
+        //    this.postCodeTrimmed = postCodeTrimmed;
+        //    this.streetName = streetName;
+        //    this.longitude = longitude;
+        //    this.latitude = latitude;
+        //    this.plusCode = plusCode;
+        //    this.region = region;
+        //    this.locality = locality;
+        //    this.area = area;
+        //    this.numUPRNs = numUPRNs;
+        //}
+    }
+}
